ea3c67db20e974e330b0b3859364288163ee6ef9
Alberto Lima <alberto.btlima@outlook.com>
2024-10-16 19:11:49 -0300

    up

---
+++ 02-explore-funcoes-e-listas/app.js
@@ -0,0 +1,28 @@
+const numeroSecreto = gerarNumeroAleatorio();
+
+function exibirTextoNaTela(tag, texto) {
+  let campo = document.querySelector(tag);
+  campo.innerHTML = texto;
+}
+
+function verificarChute() {
+  let chute = document.querySelector("input").value;
+
+  if (chute == numeroSecreto) {
+    exibirTextoNaTela("h1", "Acertou!");
+    exibirTextoNaTela("p", "Você descobriu o número secreto :)");
+  } else {
+    if (chute > numeroSecreto) {
+      exibirTextoNaTela("p", "Onúmero secreto é menor");
+    } else {
+      exibirTextoNaTela("p", "O número secreto é maior");
+    }
+  }
+}
+
+function gerarNumeroAleatorio() {
+  return parseInt(Math.random() * 10 + 1);
+}
+
+exibirTextoNaTela("h1", "Jogo do número secreto");
+exibirTextoNaTela("p", "Escolha um número entre 1 e 10");
