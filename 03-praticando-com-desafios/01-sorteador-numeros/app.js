b26d00ddd2dc08a8cd18b70fdd5d4f094513600b
Alberto Lima <alberto.btlima@outlook.com>
2024-10-17 19:15:31 -0300

    validações finais

--- 03-praticando-com-desafios/01-sorteador-numeros/app.js
+++ 03-praticando-com-desafios/01-sorteador-numeros/app.js
@@ -2,8 +2,21 @@ function sortear() {
   let quantidade = parseInt(document.getElementById("quantidade").value);
   let de = parseInt(document.getElementById("de").value);
   let ate = parseInt(document.getElementById("ate").value);
-
   let sorteados = [];
+
+  if (de >= ate) {
+    alert("'Do número' deve ser menor que 'Até o número'");
+    reiniciar();
+    alterarStatusBotao();
+    return;
+  }
+
+  if (quantidade > ate - de + 1) {
+    alert("A quantidade de números sorteados é inválida.");
+    reiniciar();
+    alterarStatusBotao();
+    return;
+  }
 
   for (let i = 0; i < quantidade; i++) {
     let numero = obterNumeroAleatorio(de, ate);
