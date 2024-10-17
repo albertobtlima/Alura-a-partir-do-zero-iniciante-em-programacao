22a74dd10e61e4545613df7a70f7a894286af683
Alberto Lima <alberto.btlima@outlook.com>
2024-10-16 22:26:00 -0300

    add: button novo jogo

--- 02-explore-funcoes-e-listas/app.js
+++ 02-explore-funcoes-e-listas/app.js
@@ -1,22 +1,37 @@
-const numeroSecreto = gerarNumeroAleatorio();
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
   let campo = document.querySelector(tag);
   campo.innerHTML = texto;
 }
 
+function exibirMensagemInicial() {
+  exibirTextoNaTela("h1", "Jogo do número secreto");
+  exibirTextoNaTela("p", "Escolha um número entre 1 e 10");
+}
+
+exibirMensagemInicial();
+
 function verificarChute() {
   let chute = document.querySelector("input").value;
 
   if (chute == numeroSecreto) {
+    let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+    let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+
     exibirTextoNaTela("h1", "Acertou!");
-    exibirTextoNaTela("p", "Você descobriu o número secreto :)");
+    exibirTextoNaTela("p", mensagemTentativas);
+    document.getElementById("reiniciar").removeAttribute("disabled");
+    document.getElementById("chutar").setAttribute("disabled", true);
   } else {
     if (chute > numeroSecreto) {
-      exibirTextoNaTela("p", "Onúmero secreto é menor");
+      exibirTextoNaTela("p", "O número secreto é menor");
     } else {
       exibirTextoNaTela("p", "O número secreto é maior");
     }
+    tentativas++;
+    limparCampo();
   }
 }
 
@@ -24,5 +39,16 @@ function gerarNumeroAleatorio() {
   return parseInt(Math.random() * 10 + 1);
 }
 
-exibirTextoNaTela("h1", "Jogo do número secreto");
-exibirTextoNaTela("p", "Escolha um número entre 1 e 10");
+function limparCampo() {
+  chute = document.querySelector("input");
+  chute.value = "";
+}
+
+function reiniciarJogo() {
+  numeroSecreto = gerarNumeroAleatorio();
+  limparCampo();
+  tentativas = 1;
+  exibirMensagemInicial();
+  document.getElementById("reiniciar").setAttribute("disabled", true);
+  document.getElementById("chutar").removeAttribute("disabled");
+}
