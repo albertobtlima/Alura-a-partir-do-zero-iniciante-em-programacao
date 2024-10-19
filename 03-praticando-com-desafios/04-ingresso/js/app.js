b9dff923dd2846509b459fafdc378406b3d0ee35
Alberto Lima <alberto.btlima@outlook.com>
2024-10-19 14:53:23 -0300

    função comprar pista

--- 03-praticando-com-desafios/04-ingresso/js/app.js
+++ 03-praticando-com-desafios/04-ingresso/js/app.js
@@ -1,4 +1,34 @@
+let qtdPista = parseInt(document.getElementById("qtd-pista").textContent);
+let qtdSuperior = document.getElementById("qtd-superior").textContent;
+let qtdInferior = document.getElementById("qtd-inferior").textContent;
+
 function comprar() {
+  let tipoDeIngreso = document.getElementById("tipo-ingresso").value;
+  let quantidade = parseInt(document.getElementById("qtd").value);
+
+  if (tipoDeIngreso == "pista") {
+    comprarPista(quantidade);
+  }
+
+  let lista = document.getElementById("lista");
+
+  lista.innerHTML = `
+    <li>Pista<span id="qtd-pista">${qtdPista}</span></li>
+    <li>Cadeira superior<span id="qtd-superior">${qtdSuperior}</span></li>
+    <li>Cadeira inferior<span id="qtd-inferior">${qtdInferior}</span></li>
+  `;
+}
+
+function comprarPista(quantidade) {
+  if (quantidade > qtdPista) {
+    alert("Quantidade indisponível para pista");
+  } else {
+    qtdPista = qtdPista - quantidade;
+    qtdPista.textContent = qtdPista;
+  }
+}
+
+/* function comprar() {
   let tipoDeIngreso = document.getElementById("tipo-ingresso").value;
   let quantidade = document.getElementById("qtd").value;
   let qtdPista = document.getElementById("qtd-pista").textContent;
@@ -20,4 +50,4 @@ function comprar() {
     <li>Cadeira superior<span id="qtd-superior">${qtdSuperior}</span></li>
     <li>Cadeira inferior<span id="qtd-inferior">${qtdInferior}</span></li>
   `;
-}
+} */
