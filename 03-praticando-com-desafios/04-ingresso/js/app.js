f379b723426784fa28b3dd0e1608638df268a21c
Alberto Lima <alberto.btlima@outlook.com>
2024-10-19 14:31:46 -0300

    função comprar

---
+++ 03-praticando-com-desafios/04-ingresso/js/app.js
@@ -0,0 +1,23 @@
+function comprar() {
+  let tipoDeIngreso = document.getElementById("tipo-ingresso").value;
+  let quantidade = document.getElementById("qtd").value;
+  let qtdPista = document.getElementById("qtd-pista").textContent;
+  let qtdSuperior = document.getElementById("qtd-superior").textContent;
+  let qtdInferior = document.getElementById("qtd-inferior").textContent;
+
+  if (tipoDeIngreso == "pista") {
+    qtdPista = qtdPista - quantidade;
+  } else if (tipoDeIngreso == "superior") {
+    qtdSuperior = qtdSuperior - quantidade;
+  } else {
+    qtdInferior = qtdInferior - quantidade;
+  }
+
+  let lista = document.getElementById("lista");
+
+  lista.innerHTML = `
+    <li>Pista<span id="qtd-pista">${qtdPista}</span></li>
+    <li>Cadeira superior<span id="qtd-superior">${qtdSuperior}</span></li>
+    <li>Cadeira inferior<span id="qtd-inferior">${qtdInferior}</span></li>
+  `;
+}
